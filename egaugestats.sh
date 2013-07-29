7057af10fd04850e763420c3887dcccaf39fffd1
Aaron Brown <aaron@9minutesnooze.com>
2013-07-29 14:23:27 -0400

    reduce sleep to 1 second less than schema interval so prevent periodic empty data.  Use bundler instead of ruby

--- egaugestats.sh
+++ egaugestats.sh
@@ -4,6 +4,6 @@ bundle check || bundle install
 
 while : ; do
   echo "$(date) Sending solar stats"
-  ruby egaugepull.rb | nc localhost 2003
-  sleep 60
+  bundle exec ./egaugepull.rb | nc localhost 2003
+  sleep 59
 done
